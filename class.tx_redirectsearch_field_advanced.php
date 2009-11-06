e18b7640589e6e00db0cdc23c09bc0b0f4c56193
karlchen321 <karlchen321@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-06 22:04:39 +0000

    remove unused Code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/redirect_search/trunk@26274 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_redirectsearch_field_advanced.php
+++ class.tx_redirectsearch_field_advanced.php
@@ -28,8 +28,6 @@
  */
 
 
-
-
 /**
  * Class/Function which manipulates the item-array for table/field tx_redirectsearch_search_advanced.
  *
@@ -39,61 +37,30 @@
  */
 class tx_redirectsearch_field_advanced {
 
-    /**
-     * [Describe function...]
-     *
-     * @param    [type]        $$params: ...
-     * @param    [type]        $pObj: ...
-     * @return    [type]        ...
-     */
-	function main(&$params,&$pObj)    {
-/*
-		debug('Hello World!',1);
-		debug('$params:',1);
-		debug($params);
-		debug('$pObj:',1);
-		debug($pObj);
-*/
+	function main(&$params,&$pObj){
+	
 		// Finding value for the path containing the template files
-		
 		$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['redirect_search']);
 
 		$readPath = t3lib_div::getFileAbsFileName($extConf['user_Files']);
 
-		// If that direcotry is valid, is a directory then select files in it:
+		// If that direcotry is valid, then select files in it:
 		if (@is_dir($readPath)){
 		
-		    //getting all HTML files in the directory:
+		    //getting all PHP files in the directory:
 		    $user_files = t3lib_div::getFilesInDir ($readPath,'php',1,1);
-		    
-		    // Start up the HTML parser:
-		    $parseHTML = t3lib_div::makeInstance ('t3lib_parseHTML');
-		    
+
 		    // Traverse that array:
 		    foreach ($user_files as $htmlFilePath) {
+			
 		        // Reset vars:
 		        $selectorBoxItem_title='';
-		        $selectorBoxItem_icon='';
+			
 		        // Reading the content of the template document ...
-		        $content = t3lib_div::getUrl ($htmlFilePath);
-		        // ... and extracting the content of the title-tags:
-		        $titles = $parseHTML->splitIntoBlock('titles',$content);
-		        
-		        $language = $BE_USER->uc["lang"] == '' ? 'default' : $BE_USER->uc["lang"];
-		        
-		        $titles_lang = $parseHTML->splitIntoBlock($language,$titles[1]);
-		        if(count($titles_lang) < 2) $titles_lang = $parseHTML->splitIntoBlock('default',$titles[1]);
-		        $titleTagContent = $parseHTML->removeFirstAndLastTag($titles_lang[1]);
-		        // Setting the item label:
-		        $selectorBoxItem_title = trim ($titleTagContent.' ('.basename($htmlFilePath).')');
-		        
-		        // Trying to look up an image icon for the template
-		        $fI = t3lib_div::split_fileref($htmlFilePath);
-		        $testImageFilename=$readPath.$fI['filebody'].'.gif';
-		        if (@is_file($testImageFilename)) $selectorBoxItem_icon = '../'.substr ($testImageFilename,strlen(PATH_site));
-		        
+		        $selectorBoxItem_title = trim('(' . basename($htmlFilePath) . ')');
+
 		        // Finally add the new item:
-		        $params["items"][] = Array($selectorBoxItem_title, basename ($htmlFilePath), $selectorBoxItem_icon);
+		        $params['items'][] = Array($selectorBoxItem_title, basename($htmlFilePath),'');
 		    }
 		}
 		// No return - the $params and $pObj variables are passed by reference, so just change content in then and it is passed back automatically...
